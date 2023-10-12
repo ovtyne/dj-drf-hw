48e64f45b995cde5b5c4a49e6f3f7cd95100a5d8
ovto <ovtyne@gmail.com>
2023-10-12 17:00:19 +0000

    hw 24.1.

--- lms/urls.py
+++ lms/urls.py
@@ -12,11 +12,11 @@ router = DefaultRouter()
 router.register(r'lms', LmsViewSet, basename='lms')
 
 urlpatterns = [
-    path('lms/create/', LessonCreateAPIView.as_view(), name='lms-create'),
-    path('lms/', LessonListAPIView.as_view(), name='lms-list'),
-    path('lms/detail/<int:pk>', LessonRetrieveAPIView.as_view(), name='lms-detail'),
-    path('lms/update/<int:pk>', LessonUpdateAPIView.as_view(), name='lms-update'),
-    path('lms/delete/<int:pk>', LessonDestroyAPIView.as_view(), name='lms-delete'),
+    path('create/', LessonCreateAPIView.as_view(), name='lms-create'),
+    path('', LessonListAPIView.as_view(), name='lms-list'),
+    path('detail/<int:pk>', LessonRetrieveAPIView.as_view(), name='lms-detail'),
+    path('update/<int:pk>', LessonUpdateAPIView.as_view(), name='lms-update'),
+    path('delete/<int:pk>', LessonDestroyAPIView.as_view(), name='lms-delete'),
 ] + router.urls
 
 router = SimpleRouter()
