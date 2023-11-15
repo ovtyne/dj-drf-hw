06bc9e8ed3df74f7ce54947da1b27026dc5bf0bc
ovto <ovtyne@gmail.com>
2023-11-15 18:21:13 +0400

    hw 27.2

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,8 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'db'
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
